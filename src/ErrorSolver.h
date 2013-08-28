7d6afc4a1cfd816c5c1c175f323daeee45a542c5
Jan Navratil <inavratil@fit.vutbr.cz>
2013-08-28 19:10:51 +0200

    Pridan kod pro vypocet posunuti v bodech mrizky (zatim jako extra program)
    Program uveden do puvodniho stavu, kdy hodnoty v texturach znamenaji neco
    rozumneho.

---
+++ src/ErrorSolver.h
@@ -0,0 +1,22 @@
+#ifndef _ErrorSolver_H_
+#define _ErrorSolver_H_
+
+class ErrorSolver
+{ 
+
+//-----------------------------------------------------------------------------
+// -- Member variables
+
+protected:
+
+//-----------------------------------------------------------------------------
+//-- Public methods 
+
+public:
+	ErrorSolver(void);
+	virtual ~ErrorSolver(void);
+
+};
+
+#endif
+
