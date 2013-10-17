83e7391329a87da586749a32dde9e983ceb42d31
Jan Navratil <inavratil@fit.vutbr.cz>
2013-10-17 09:54:03 +0200

    upraveny a fungujici model auta

--- src/DPSMApp.cpp
+++ src/DPSMApp.cpp
@@ -25,7 +25,7 @@ void DPSMApp::CreateContent()
 	TLight* light = m_scene->AddLight( 0, dgrey, white, white, glm::vec3( 0.0, 0.0, 0.0 ), 1e4f );
 	light->Move(  glm::vec3( -89, 55,125 ) );
 
-	m_scene->LoadScene("data/obj/scenes/car.3ds");
+	m_scene->LoadScene("data/obj/scenes/car2.3ds");
 
 	m_scene->SetFreelookCamera( glm::vec3(-13.1099,7.05098,-207.398 ), glm::vec3(0, 1, 0), glm::vec3(-13.1099,7.05098,-207.398)+glm::vec3(0, 0, -1000) );
 	m_scene->AdjustFreelookCamera(-4,-169);
